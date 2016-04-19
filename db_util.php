ea3c4c0bd34ca47516e35a77e5e6fdd4e4e174b3
Tata201201 <tata201201@gmail.com>
2016-04-19 14:24:24 +0700

    add get all reviews

--- db_util.php
+++ db_util.php
@@ -8,7 +8,7 @@ function dbUtil_connect(){
     }
     return isset($db);
 }
-function dbUtil_getAllLocations(&$qh){
+function dbUtil_getAllLocations(){
     global $db;
     if(!dbUtil_connect()){
         return false;
@@ -18,7 +18,24 @@ function dbUtil_getAllLocations(&$qh){
         $return_result=array();
         $counter = 0;
         while($row = mysqli_fetch_array($result)){
-            $return_result[$counter]=array("name" => $row['name'], "description" => $row['description'], "lat" => $row[lat], "lng" => $row['lng']);
+            $return_result[$counter]=array("id" => $row['id'], "name" => $row['name'], "description" => $row['description'], "lat" => $row['lat'], "lng" => $row['lng']);
+            $counter++;
+        }
+        return json_encode($return_result);
+    }
+}
+function dbUtil_getAllReviews($location_id){
+    global $db;
+    if(!dbUtil_connect()){
+        return false;
+    }else{
+        if(!is_numeric($location_id)) return false;
+        $sql = "SELECT * FROM reviews WHERE location_id = '$location_id'";
+        $result = mysqli_query($db,$sql);
+        $return_result=array();
+        $counter = 0;
+        while($row = mysqli_fetch_array($result)){
+            $return_result[$counter]=array("id" => $row['id'], "user_id" => $row['user_id'], "location_id" => $row['location_id'], "star" => $row['star'], "description" => $row['description']);
             $counter++;
         }
         return json_encode($return_result);
--- test.php
+++ test.php
@@ -1,4 +1,6 @@
 <?php
 include('db_util.php');
-print("dbUtil_getAllLocations() : " . dbUtil_getAllLocations());
+print("dbUtil_getAllLocations() : " . dbUtil_getAllLocations() . "<br>");
+print("dbUtil_getAllReviews('ABC') : " . dbUtil_getAllReviews('ABC') . "<br>");
+print("dbUtil_getAllReviews(1) : " . dbUtil_getAllReviews(1) . "<br>");
 ?>
