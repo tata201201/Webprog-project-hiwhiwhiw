5de0d78c3288dfefe1bcc9e85e49f01571b1e4c9
Tata201201 <tata201201@gmail.com>
2016-04-19 15:52:23 +0700

    debug

--- test.php
+++ test.php
@@ -5,8 +5,8 @@ print("dbUtil_getAllReviewsByLocation('ABC') : " . dbUtil_getAllReviewsByLocatio
 print("dbUtil_getAllReviewsByLocation(1) : " . dbUtil_getAllReviewsByLocation(1) . "<br>");
 print("dbUtil_checkLogin('test','test') : " . dbUtil_checkLogin('test','test') . "<br>");
 print("dbUtil_checkLogin('test','test1') : " . dbUtil_checkLogin('test','test1') . "<br>");
-print("dbUtil_getAllReviewsByUser('ABC') : " . dbUtil_getAllReviewsByUser('ABC') . "<br>");
-print("dbUtil_getAllReviewsByUser(1) : " . dbUtil_getAllReviewsByUser(1) . "<br>");
-print("dbUtil_getUserInfo('ABC') : " . dbUtil_getUserInfo('ABC') . "<br>");
-print("dbUtil_getUserInfo(1) : " . dbUtil_getUserInfo(1) . "<br>");
+//print("dbUtil_getAllReviewsByUser('ABC') : " . dbUtil_getAllReviewsByUser('ABC') . "<br>");
+//print("dbUtil_getAllReviewsByUser(1) : " . dbUtil_getAllReviewsByUser(1) . "<br>");
+//print("dbUtil_getUserInfo('ABC') : " . dbUtil_getUserInfo('ABC') . "<br>");
+//print("dbUtil_getUserInfo(1) : " . dbUtil_getUserInfo(1) . "<br>");
 ?>
