27c3c24c8b1c23b8559720ae7d48baa36bc12d79
benps <bentops@gmail.com>
2016-04-20 00:04:57 +0700

    change to bootstrap

--- js/login.js
+++ js/login.js
@@ -1,24 +1,46 @@
 $(document).ready(function()
 {
+	$("input").keyup(function(event){
+	    if(event.keyCode == 13){
+	    	login();
+	    }
+	});
 	$(".login-button").click(function() {
-		var username = $("#username").val();
+		login();
+	});
+});
+
+function login() {
+	console.log("login");
+	var username = $("#username").val();
 		var password = $("#password").val();
 		console.log(username);
-		// $.ajax
-		// ({
-		// 	url: "",
-		// 	type: "post",
-		// 	data: 
-		// 	{
-		// 		'username': username,
-		// 		'password': password
-		// 	},
-		// 	dataType: "json"
-		// }).done(function(data) {
-		// 	if(data) {
-				$(".mdl-layout__header.is-casting-shadow").css("background-color", "black");
-				$(".mdl-layout__tab-bar").css("background-color", "black");
-    			//data.color
+		$.ajax
+		({
+			url: "../command_request.php?command=check_login",
+			type: "post",
+			data: 
+			{
+				'username': username,
+				'password': password
+			},
+			dataType: "json"
+		}).done(function(data) {
+			if(data) {
+				$.ajax
+				({
+					url: "../command_request.php?command=get_user_info",
+					type: "post",
+					data: 
+					{
+						'user_id': data
+					},
+					dataType: "json"
+				}).done(function(data) {
+					$(".layout__header").css("background-color", data.color);
+					console.log(data);
+					console.log(data.color);
+				});
 				$("#login-tab").text(username + "'s PROFILE");
 				$("#locations-tab").removeClass("disabled");
 				$("#reviews-tab").removeClass("disabled");
@@ -27,11 +49,16 @@ $(document).ready(function()
     //             	$(".mdl-grid.login").append(data);
 		  //   		$("#profile-username").text(username);
     //         	});
-		    	$(".mdl-grid.login").load("../admin/profile.html", function() {
+		    	$(".login").load("../admin/profile.html", function() {
 		    		$("#profile-username").text(username);
 		    	});
-		    	
-		// 	}
-		// });
-	});
-});
+		    	$("#fixed-tab-2 > .page-content").load("../admin/locations.html", function() {
+		    		// $("#profile-username").text(username);
+		    	});
+		    	$("#fixed-tab-3 > .page-content").load("../admin/reviews.html", function() {
+		    		// $("#profile-username").text(username);
+		    	});
+
+			}
+		});
+}
