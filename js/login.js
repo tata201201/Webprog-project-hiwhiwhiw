d682b6c4a74a2f4abcbf2642628ec5f9325ae5ce
benps <bentops@gmail.com>
2016-04-19 17:35:31 +0700

    ทำหน้า profile

--- js/login.js
+++ js/login.js
@@ -3,17 +3,35 @@ $(document).ready(function()
 	$(".login-button").click(function() {
 		var username = $("#username").val();
 		var password = $("#password").val();
-		// var loginSuccess = ....;
 		console.log(username);
-		if(loginSuccess) {
-			$("#login-tab").text(username + "'s PROFILE");
-			$("#locations-tab").removeClass("disabled");
-			$("#reviews-tab").removeClass("disabled");
-			$(".loginfield").remove();
-        	$(".mdl-grid .login").append("<li>Appended item</li>");
-		}
-		else {
-
-		}
+		// $.ajax
+		// ({
+		// 	url: "",
+		// 	type: "post",
+		// 	data: 
+		// 	{
+		// 		'username': username,
+		// 		'password': password
+		// 	},
+		// 	dataType: "json"
+		// }).done(function(data) {
+		// 	if(data) {
+				$(".mdl-layout__header.is-casting-shadow").css("background-color", "black");
+				$(".mdl-layout__tab-bar").css("background-color", "black");
+    			//data.color
+				$("#login-tab").text(username + "'s PROFILE");
+				$("#locations-tab").removeClass("disabled");
+				$("#reviews-tab").removeClass("disabled");
+				$(".loginfield").remove();
+				// $.get("../admin/profile.html",function(data){
+    //             	$(".mdl-grid.login").append(data);
+		  //   		$("#profile-username").text(username);
+    //         	});
+		    	$(".mdl-grid.login").load("../admin/profile.html", function() {
+		    		$("#profile-username").text(username);
+		    	});
+		    	
+		// 	}
+		// });
 	});
 });
