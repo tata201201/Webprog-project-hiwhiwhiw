5281cb6f1a97c4c32f32e1db4afbd967ce655a98
Tata201201 <tata201201@gmail.com>
2016-04-21 22:03:10 +0700

    debug

--- db_util.php
+++ db_util.php
@@ -194,7 +194,7 @@ function dbUtil_addReview($user_id, $location_id, $star, $description, $photos){
         $result = mysqli_query($db,$sql);
         if($result->num_rows == 1){
             $row = mysqli_fetch_array($result);
-            echo "<script>alert(" . count($photos) . ")</script>";
+            //echo "<script>alert(" . count($photos) . ")</script>";
             for($i=0;$i<count($photos);$i++){
                 if($photos[i] == "") continue;
                 $sql = "INSERT INTO photos (directory) VALUES ('$photos[i]')";
