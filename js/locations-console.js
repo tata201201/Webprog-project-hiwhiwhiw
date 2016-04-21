c9e0de883fa5980333a7b855ebb69b79c550f8d6
benps <bentops@gmail.com>
2016-04-22 03:45:32 +0700

    แก้ textarea ...

--- js/locations-console.js
+++ js/locations-console.js
@@ -5,8 +5,26 @@ $(document).ready(function()
 		var lo_id = this.id;
 		$(".reviewsfield").remove();
 		$(".login").load("../admin/add-review.html", function() {
-			$("#location-id").val(lo_id);
-			$.getScript("../js/add-review.js");
+			$.ajax
+			({
+				url: "../command_request.php?command=get_location_by_id",
+				type: "post",
+				data: 
+				{
+					'location_id': lo_id
+				},
+				dataType: "json"
+			}).done(function(data) {
+				$("#location-id").val(lo_id);
+				$("p").html("Location's id: "+"<br/>"+lo_id);
+				$("h5").html("Location's name: "+"<br/>"+data.name);
+				$(".tab-2").removeClass("selected");
+				tabstate = 3;
+				$(".tab-3").addClass("selected");
+				$.getScript("../js/add-review.js");
+			}).fail(function() {
+				console.log("fail get lo by id");
+			});		
     	});
 	});
 	$(".edit-location-button").click(function() {
@@ -32,9 +50,7 @@ $(document).ready(function()
 	    	});
 		}).fail(function() {
 			console.log("fail edit");
-		});
-
-		
+		});		
 	});
 	$(".delete-location-button").click(function() {
 		$.ajax
