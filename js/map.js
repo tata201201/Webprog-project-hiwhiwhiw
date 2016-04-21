23205d98cee5d0ce57ada7abef8409d6d22fa0bd
filmzuri94 <filmzur94@gmail.com>
2016-04-22 01:55:05 +0700

    add reviewer's color

--- js/map.js
+++ js/map.js
@@ -86,12 +86,14 @@ function display_review_pane(location, reviews){
 
 
   for(var i = 0;i<reviews.length;i++){
-    show_reviews = "<h1>Review:</h1><div id='review_user'> <b>By: "+reviews[i].user_name+"</b></div>";
+    var color_user = "review_user_"+i;
+    show_reviews = "<div id='review_user_"+i+"'> <b>By: "+reviews[i].user_name+"</b></div>";
     show_reviews = show_reviews+"<div id='review_star'> Star:"+"<div class='progress'>"+
     "<div class='progress-bar' role='progressbar' aria-valuenow='3' aria-valuemin='0' aria-valuemax='5' style='width:"+(reviews[i].star*20)+"%'>"+
     "</div></div></div>";
     show_reviews = show_reviews+"<div id='review_description'> Review: <br>"+reviews[i].description+"</div>";
     $(show_reviews).appendTo("#review_details");
+    $("#"+color_user).css("background-color",reviews[i].color);
   }
 
 
